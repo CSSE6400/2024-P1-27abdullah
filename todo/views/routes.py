ee09c6de2733b3c6338e9f18f9f8f68f474691db
27abdullah <98884418+27abdullah@users.noreply.github.com>
2024-02-21 06:19:14 +0000

    bug

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -39,19 +39,7 @@ def create_todo():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }, 201)
-
-@api.route('/todos/<int:id>', methods=['PUT'])
-def update_todo(id):
-    return jsonify({
-    "id": id,
-    "title": "Watch CSSE6400 Lecture",
-    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-    "completed": True,
-    "deadline_at": "2023-02-27T00:00:00",
-    "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
-    })
+    }, 202)
 
 
 @api.route('/todos/<int:id>', methods=['PUT'])
